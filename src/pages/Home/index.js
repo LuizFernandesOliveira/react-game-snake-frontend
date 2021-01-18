af905cf3f1ef2078a4b1106247f53b41c15de4f6
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-01-18 20:08:21 -0300

    install and implements styled-components

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Container, Content,
+} from './styles';
+
+function Home() {
+  return (
+    <Container>
+      <Content>
+        Snake Game
+      </Content>
+    </Container>
+  )
+}
+
+export default Home;
